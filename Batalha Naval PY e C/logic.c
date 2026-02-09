411322d1a688da05d93c8b1e2586511469db4071
Mateus Antunes <mateusetec424@gmail.com>
2026-02-09 20:00:28 -0300

    Create logic.c

---
+++ Batalha Naval PY e C/logic.c
@@ -0,0 +1,14 @@
+//LÓGICA DE BATALHA NAVAL EM C INTEGRAÇÃO COM PHYTON
+
+#include <stdio.h>
+
+int processar_ataque_c(int x, int y, int tamanho, char grid[tamanho][tamanho]) {
+    if (grid[x][y] == 'N') {
+        return 1;
+    } else if (grid[x][y] == 'X' || grid[x][y] == 'O') {
+        return 2;
+    }
+    return 0:
+} 
+
+
